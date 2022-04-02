2a124bc275feeef85810d6e771fa621642e6edc7
leanhvu21042001 <leanhvu21042001@gmail.com>
2022-04-02 09:33:17 +0700

    fix: update translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,18 @@
 import i18n from "i18next";
-import i18next from "i18next";
 import Backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-i18next
+i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
-    debug: false,
-    detection: {
-      order: ["queryString", "cookie"],
-      cache: ["cookie"],
-    },
+    debug: true,
+
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react!!
     },
   });
 
