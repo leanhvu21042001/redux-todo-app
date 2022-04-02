692676caa0675491d3da3b8b0dbb6b7226f62aaa
leanhvu21042001 <leanhvu21042001@gmail.com>
2022-04-02 10:09:44 +0700

    fix(i18n.js): set debug to false

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import translationVN from "./locales/vn/translation.json";
 import translationEN from "./locales/en/translation.json";
@@ -21,7 +21,7 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
-    debug: true,
+    debug: false,
     lng: "en",
     resources,
     interpolation: {
